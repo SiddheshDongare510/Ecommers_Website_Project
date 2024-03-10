ccf0e94143d9412b1a5a3f81299eb8ed4bee7bf7
Siddhesh Dongare <siddheshdongare1234@gmail.com>
2024-03-10 15:27:11 +0530

    register api and middlewares

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
-import { hashPassword } from "../helpers/authHelper.js";
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
 import userModel from "../models/userModel.js";
+import JWT from "jsonwebtoken";
 
 export const registerController = async (req,res) => {
     try{
@@ -32,7 +33,7 @@ export const registerController = async (req,res) => {
         //register user
         const hashedPassword = await hashPassword(password);
         //save
-        const user = new userModel({name,email,phone,address,password:hashedPassword}).save();
+        const user = await new userModel({name,email,phone,address,password:hashedPassword}).save();
 
         res.status(201).send({
             success:true,
@@ -48,3 +49,59 @@ export const registerController = async (req,res) => {
         });
     }
 };
+
+//post login
+export const loginController = async (req,res) => {
+    try{
+        const {email,password} = req.body;
+        //validation
+        if(!email || !password){
+            return res.status(404).send({
+                success:false,
+                message:"Invalid email or password"
+            })
+        }
+        //check user
+        const user = await userModel.findOne({email});
+        if(!user){
+            return res.status(404).send({
+                success:false,
+                message:"Email is not registered"
+            })
+        }
+        const match = await comparePassword(password,user.password)
+        if(!match){
+            return res.status(200).send({
+                success:false,
+                message:"Invalid Password"
+            })
+        }
+        const token = await JWT.sign({_id:user._id},process.env.JWT_SECRET,
+            {expiresIn:"7d",
+        });
+        res.status(200).send({
+            success:true,
+            message:"LogIn Successfully",
+            user:{
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                address: user.address,
+            },
+            token,
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message: "Error in Login",
+            error,
+        })
+    }
+};
+
+//test controller
+export const testController = (req,res) => {
+    res.send("Protected Route");
+}
