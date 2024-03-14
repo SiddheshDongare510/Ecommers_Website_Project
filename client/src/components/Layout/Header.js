3ea257daaf51c27ea8084e995a991d53194ffd6f
Siddhesh Dongare <siddheshdongare1234@gmail.com>
2024-03-14 12:20:38 +0530

    context api setup

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {NavLink, Link} from 'react-router-dom'
 import { useAuth } from '../../context/auth';
 import toast from 'react-hot-toast';
-import TiShoppingCart from './TiShoppingCart';
 
 const Header = () => {
   const [auth,setAuth] = useAuth()
@@ -21,7 +20,7 @@ const Header = () => {
       <span className="navbar-toggler-icon">
       </span></button>
     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <Link to="/" className="navbar-brand" > <TiShoppingCart /> Ecommerce App</Link>
+      <Link to="/" className="navbar-brand" > Ecommerce App</Link>
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <NavLink to="/" className="nav-link " >Home</NavLink>
