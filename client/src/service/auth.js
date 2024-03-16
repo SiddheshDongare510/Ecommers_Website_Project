e8ff4aedcb1cfefa66a93ff81a34fe167dfb7781
Nishtha Jaiswal <nishthajaiswal@gmail.com>
2024-03-17 04:05:07 +0530

    created product

--- client/src/service/auth.js
+++ client/src/service/auth.js
@@ -1,6 +1,13 @@
-
+import React, { useState } from "react";
 import ForgotPassword from "../pages/Auth/ForgotPassword";
+import CreateCategory from './../pages/Admin/CreateCategory';
 const  auth_base = "http://localhost:8080/api/v1/auth";
+const get_base ="http://localhost:8080/api/v1/category";
+//const update_base = `http://localhost:8080/api/v1/category/${selected._id }`;
+
+
+
+
 
 export const authEndPoints = {
     register: auth_base + "/register",
@@ -8,4 +15,9 @@ export const authEndPoints = {
     forgotPassword: auth_base + "/forgot-password",
     userAuth : auth_base + "/user-auth",
     adminAuth : auth_base + "/admin-auth",
+    getCategory :get_base + "/get-category",
+    createCategory:get_base +"/create-category",
+   // updateCategory :update_base +"/update-category",
+
 }
+
